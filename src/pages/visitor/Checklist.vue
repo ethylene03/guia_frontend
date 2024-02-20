5f13dc8291b566e6e999a098b574bc8611f19561
Princess Ethel <pmatillo@up.edu.ph>
2024-02-20 08:31:45 +0800

    Update Checklist.vue

--- src/pages/visitor/Checklist.vue
+++ src/pages/visitor/Checklist.vue
@@ -2,10 +2,27 @@
     import Header from '@/assets/components/Header.vue';
     import Footer from '@/assets/components/Footer.vue';
 
+    // pseudo image
+    import artImage from '@/assets/images/artwork.png'
+
     export default {
         components: {
             Header,
             Footer,
+        },
+
+        data() {
+            return {
+                artworks: [
+                    {id: 1, name: 'Lorem Ipsum1', year: '1967', artist: 'Ellenmarie'},
+                    {id: 2, name: 'Lorem Ipsum2', year: '1967', artist: 'Ellenmarie'},
+                    {id: 3, name: 'Lorem Ipsum3', year: '1967', artist: 'Ellenmarie'},
+                    {id: 4, name: 'Lorem Ipsum4', year: '1967', artist: 'Ellenmarie'},
+                    {id: 5, name: 'Lorem Ipsum5', year: '1967', artist: 'Ellenmarie'},
+                    {id: 6, name: 'Lorem Ipsum6', year: '1967', artist: 'Ellenmarie'},
+                    {id: 7, name: 'Lorem Ipsum7', year: '1967', artist: 'Ellenmarie'},
+                ]
+            }
         }
     }
 </script>
@@ -15,8 +32,17 @@
         <Header type="user" :isMap="false" />
 
         <div class="content">
-            <!-- insert page contents here -->
-            Checklist
+            <!-- subheader -->
+            <h1>Artwork Checklist</h1>
+            <h1>Section II</h1>
+
+            <!-- checklist -->
+            <div class="checklist">
+                <div class="list" v-for="art in artworks">
+                    <input :id="art.id" type="checkbox" />
+                    <text style="font-weight: bold;">{{ art.name }} ({{ art.year }}) by {{ art.artist }}</text>
+                </div>
+            </div>
         </div>
 
         <Footer />
@@ -26,6 +52,36 @@
 <style scoped>
     .content {
         width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-bottom: 3.2rem;
+    }
+
+    .checklist {
+        border-radius: 5px;
+        border: 2px solid var(--color-secondary);
+        width: 100%;
+        height: 65vh;
+        margin-top: 20px;
+
+        overflow-y: auto;
+    }
+
+    .list {
+        border-bottom: 1px solid rgba(0, 0, 0, 0.53);
+        padding: 10px 0 10px 20px;
+
+        display: flex;
+        align-items: center;
+    }
+
+    .list input {
+        margin-right: 10px;
+    }
+    
+    .list input:checked {
+        accent-color: var(--color-secondary);
     }
 
     /* CSS STYLING FOR RESPONSIVENESS */
