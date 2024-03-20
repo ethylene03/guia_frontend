5855ecd2e84bd216afdf1db8cf862345afe3ec08
Princess Ethel <pmatillo@up.edu.ph>
2024-03-20 22:30:10 +0800

    automate test running

---
+++ run-tests.js
@@ -0,0 +1,19 @@
+import fs from 'fs';
+import { exec } from 'child_process';
+
+fs.readdir('./test_cases', (err, files) => {
+  if (err) throw err;
+
+  const sideFiles = files.filter(file => file.endsWith('.side'));
+  const command = `selenium-side-runner ${sideFiles.map(file => `./test_cases/${file}`).join(' ')}`;
+
+  exec(command, (err, stdout, stderr) => {
+    if (err) {
+      console.error(`exec error: ${err}`);
+      return;
+    }
+
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+  });
+});
