503daf3910dc082557ecd046254f2157330a38a8
Princess Ethel <pmatillo@up.edu.ph>
2024-04-01 12:29:04 +0800

    Update run-tests.js

--- run-tests.js
+++ run-tests.js
@@ -5,7 +5,7 @@ fs.readdir('./test_cases', (err, files) => {
   if (err) throw err;
 
   const sideFiles = files.filter(file => file.endsWith('.side'));
-  const command = `selenium-side-runner ${sideFiles.map(file => `./test_cases/${file}`).join(' ')}`;
+  const command = `selenium-side-runner -c "browserName=chrome chromeOptions.args=[--headless=new,--disable-gpu]" ${sideFiles.map(file => `./test_cases/${file}`).join(' ')}`;
 
   exec(command, (err, stdout, stderr) => {
     if (err) {
